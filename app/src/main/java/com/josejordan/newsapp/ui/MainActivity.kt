57c94109a51e57f9b3b0b24678b56afb68a89713
Jose Jordan <mundodigital.pro@gmail.com>
2023-06-01 12:10:13 +0200

    Flow Coroutines

--- app/src/main/java/com/josejordan/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/josejordan/newsapp/ui/MainActivity.kt
@@ -2,13 +2,18 @@ package com.josejordan.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.josejordan.newsapp.viewmodel.NewsViewModel
 import com.josejordan.newsapp.databinding.ActivityMainBinding
 import com.josejordan.newsapp.utils.Constants
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -27,7 +32,10 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         bindAdapterToRecyclerView(binding.recyclerView)
 
-        observeNewsLiveData()
+        //observeNewsLiveData()
+        observeNewsFlow()
+        observeErrorFlow()
+
 
         fetchTopHeadlines(Constants.COUNTRY, Constants.API_KEY)
     }
@@ -40,11 +48,34 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = newsAdapter
     }
 
-    private fun observeNewsLiveData() {
+/*    private fun observeNewsLiveData() {
         newsViewModel.newsLiveData.observe(this) { news ->
             newsAdapter.submitList(news)
         }
+    }*/
+    private fun observeNewsFlow() {
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                newsViewModel.newsFlow.collect { news ->
+                    newsAdapter.submitList(news)
+                }
+            }
+        }
     }
+
+    private fun observeErrorFlow() {
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                newsViewModel.errorFlow.collect { error ->
+                    error?.let {
+                        Toast.makeText(this@MainActivity, it, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+
 
     private fun fetchTopHeadlines(country: String, apiKey: String) {
         newsViewModel.getTopHeadlines(country, apiKey)
